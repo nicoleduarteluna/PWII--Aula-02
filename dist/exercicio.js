1ede1cff6f273c73793934118108b0e446b59752
niluiz <nicoleduarteluna@hotmail.com>
2019-08-07 21:58:59 -0300

    Exercício das médias

---
+++ dist/exercicio.js
@@ -0,0 +1,9 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 5;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A média das notas é " + media);
+if (media >= 7)
+    console.log("Você está aprovado");
+else
+    console.log("Você está reprovado");
